112d194bf3bedf0cefa224b3ac28f9b9e2f9962f
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-11 11:53:47 +0530

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static long factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number greater than or equal to zero:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factorial of  "+n+" is:"+factorial(n));
+        }
+    }
+}
